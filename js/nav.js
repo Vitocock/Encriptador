3e16bb56dfa6111f46b96c0d46cbed836ca3c1a8
Victor Morales <vmmc100@gmail.com>
2022-05-01 20:51:23 -0400

    animacion navBar

---
+++ js/nav.js
@@ -0,0 +1,11 @@
+/* const navBtn = document.getElementById('nav-button')
+const navList = document.getElementById('nav-bar')
+
+navBtn.addEventListener('click', (event) => {
+
+    if (navList.classList.contains('ocultar')) {
+        navList.classList.remove('ocultar')
+    } else {
+        navList.classList.add('ocultar')
+    }
+}) */
